d85cded8db599cbdcea58f010ad912435de5143c
SuryanshK <suryansh.khranger@gmail.com>
2026-07-12 21:01:29 -0600

    added yaml for github actions to publish nextJS site. see .github/workflows/nextjs.yml

--- _next/static/chunks/app/test/page-353509712d90563a.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[928],{3007:function(e,r,n){Promise.resolve().then(n.bind(n,5897))},622:function(e,r,n){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var t=n(2265),o=Symbol.for("react.element"),f=Symbol.for("react.fragment"),_=Object.prototype.hasOwnProperty,s=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function c(e,r,n){var t,f={},c=null,i=null;for(t in void 0!==n&&(c=""+n),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(i=r.ref),r)_.call(r,t)&&!u.hasOwnProperty(t)&&(f[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===f[t]&&(f[t]=r[t]);return{$$typeof:o,type:e,key:c,ref:i,props:f,_owner:s.current}}r.Fragment=f,r.jsx=c,r.jsxs=c},7437:function(e,r,n){"use strict";e.exports=n(622)}},function(e){e.O(0,[897,971,864,744],function(){return e(e.s=3007)}),_N_E=e.O()}]);
